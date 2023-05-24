d42c68ab7437a96cf1a2766dde5a39f8b50328c6
Donecker <130527889+Donecker@users.noreply.github.com>
2023-05-23 22:26:26 -0700

    Fixed fs

--- libfs/fs.c
+++ libfs/fs.c
@@ -47,7 +47,7 @@ int fs_mount(const char *diskname)
 	//Make sure to check data before initializing superblock
 
 	//Allocate memory for superblock
-	struct superblock* sb = (struct superblock*)malloc(sizeof(struct superblock));
+	sb = (struct superblock*)malloc(sizeof(struct superblock));
 
 	//Variable to hold the block you read from
 	//uint8_t block[BLOCK_SIZE]; Might user later
@@ -58,35 +58,16 @@ int fs_mount(const char *diskname)
 	}
 
 	//Setup signature to compare to
-	char* defaultSig = "ECS150FS";
+	char* default_sig = "ECS150FS";
 
 	//Check if block has correct signature
-	if (memcmp(&sb->signature,defaultSig,sizeof(uint64_t))) {
+	if (memcmp(&sb->signature,default_sig,sizeof(uint64_t))) {
 		printf("Signatures do not match\n");
 		return -1;
 	}
 	printf("Signatures do match!\n");
 
-	/*
-
-	sb->signature = (uint64_t)"ECS150FS";
-
-	uint16_t num_data_blocks = block_disk_count();
-	uint8_t num_fat_blocks = num_data_blocks * 2 / BLOCK_SIZE;
-	uint16_t root_index = num_fat_blocks + 1;
-	uint16_t data_start_index = root_index + 1;
-	uint16_t total_blocks = 2 + num_data_blocks + num_fat_blocks;
-
-	sb->total_blocks = total_blocks;
-	sb->root_index = root_index;
-	sb->data_start_index = data_start_index;
-	sb->num_data_blocks = num_data_blocks;
-	sb->num_fat_blocks = num_fat_blocks;
-
-	*/
-
-	//block_write(0, sb);
-
+	printf("\n");
 	return 0;
 }
 
@@ -108,8 +89,8 @@ int fs_info(void)
 	printf("FS Info:\n");
 	printf("total_blk_count=%d\n", sb->total_blocks);
 	printf("fat_blk_count=%d\n", sb->num_fat_blocks);
-	//printf("rdir_blk=%d\n", sb->); not from superblock
-	//printf("data_blk=%d\n", sb->); not from superblock
+	printf("rdir_blk=%d\n", sb->num_fat_blocks+1); 
+	printf("data_blk=%d\n", sb->num_fat_blocks + 2); 
 	printf("data_blk_count=%d\n", sb->num_data_blocks);
 	//printf("fat_free_ratio=%d\n", sb->); not from superblock
 	//printf("rdir_free_ratio%d\n", sb->); not from superblock
