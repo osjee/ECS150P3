13e8f356dfd068aafb1e60a29456714098b6cff1
Donecker <130527889+Donecker@users.noreply.github.com>
2023-06-03 23:20:30 -0700

    Many bug fixes

--- libfs/fs.c
+++ libfs/fs.c
@@ -11,7 +11,6 @@ typedef __uint128_t uint128_t;
 #define ROOT_ENTRIES 128
 #define FAT_EOC 0xFFFF
 
-/* TODO: Phase 1 */
 struct superblock {
 	uint64_t signature;
 	uint16_t total_blk_count;
@@ -49,13 +48,13 @@ int used_root_entries;
 
 int fs_open_count; // Track how many files are open
 
-struct file_descripter {
+struct file_descriptor {
 	int root_entry; 
 	size_t offset;
 	const char* filename;
 };
 
-struct file_descripter *files[FS_FILE_MAX_COUNT];
+struct file_descriptor *files[FS_FILE_MAX_COUNT];
 
 // Helper functions
 
@@ -64,17 +63,23 @@ int get_free_data_block() {
 
 	int block, index;
 	int stop_flag = 0;
+	int data_blk_counter = 0;
 
 	for (block = 0; block < sb->fat_blk_count; block++) {
 		for (index = 0; index < BLOCK_SIZE / 2; index++) {
+			if (data_blk_counter == sb->data_blk_count) {
+				return -1;
+			}
 			if (!fat_array[block].index[index]) {
 				stop_flag = 1;
 				break;
 			}
+			data_blk_counter += 1;
 		}
 		if (stop_flag) {
 			break;
 		}
+		
 	}
 
 	if (!stop_flag) {
@@ -101,9 +106,47 @@ void print() {
 	}
 }
 
+int isCorrupt() {
+	//Setup signature to compare to
+	char* default_sig = "ECS150FS";
+
+	//Check if block has correct signature
+	if (memcmp(&sb->signature, default_sig, sizeof(uint64_t))) {
+		//printf("Signatures do not match\n");
+		return -1;
+	}
+
+	//Check for correct total block count
+	if (sb->total_blk_count != 1 + 1 + sb->fat_blk_count + sb->data_blk_count) {
+		return -1;
+	}
+
+	//Check for correct root index
+	if (sb->rdir_blk != sb->fat_blk_count + 1) {
+		return -1;
+	}
+
+	//Check for correct data block index
+	if (sb->data_blk != sb->rdir_blk + 1) {
+		return -1;
+	}
+
+	//Check for corrent amount of fat blocks
+	if (sb->fat_blk_count != (sb->data_blk_count * 2) / BLOCK_SIZE) {
+
+		//Basically a cieling operation
+		if ((sb->data_blk_count * 2) % BLOCK_SIZE != 0 && sb->fat_blk_count - ((sb->data_blk_count * 2) / BLOCK_SIZE) == 1) {
+			return 0;
+		}
+
+		return -1;
+	}
+
+	return 0;
+}
+
 int fs_mount(const char *diskname)
 {
-	/* TODO: Phase 1 */
 	block_disk_open(diskname);
 
 	//Make sure to check data before initializing superblock
@@ -150,6 +193,11 @@ int fs_mount(const char *diskname)
 	}
 	//printf("Signatures do match!\n");
 
+	//This will check the superblock for corruption
+	if (isCorrupt() == -1) {
+		return -1;
+	}
+
 	used_fat_entries = 0;
 	
 	//Read through all fat blocks to determine how many are used	
@@ -182,7 +230,7 @@ int fs_mount(const char *diskname)
 	}
 
 	//Initialize files, dont allocate memory to the file?
-	//files = malloc(FS_FILE_MAX_COUNT * sizeof(struct file_descripter));
+	//files = malloc(FS_FILE_MAX_COUNT * sizeof(struct file_descriptor));
 
 	for (int i = 0; i < FS_FILE_MAX_COUNT; i++) {
 		files[i] = NULL;
@@ -193,8 +241,6 @@ int fs_mount(const char *diskname)
 
 int fs_umount(void)
 {
-	/* TODO: Phase 1 */
-
 	// Writing to block
 	block_write(0, sb);
 	for (int i = 0; i < sb->fat_blk_count; i++) {
@@ -219,7 +265,18 @@ int fs_umount(void)
 
 int fs_info(void)
 {
-	/* TODO: Phase 1 */
+	used_fat_entries = 0;
+
+	//Read through all fat blocks to determine how many are used	
+	for (int i = 0; i < sb->fat_blk_count; i++) {
+		for (int j = 0; j < BLOCK_SIZE / 2; j++) {
+
+			//If the entry is 0 that means it is empty
+			if (fat_array[i].index[j]) {
+				used_fat_entries++;
+			}
+		}
+	}
 
 	printf("FS Info:\n");
 	printf("total_blk_count=%d\n", sb->total_blk_count);
@@ -235,8 +292,6 @@ int fs_info(void)
 
 int fs_create(const char *filename)
 {
-	/* TODO: Phase 2 */
-
 	// Return -1 if no FS is mounted or filename is invalid or filename length is too long or root directory contains maximum file count
 	if (block_disk_count() == -1 || !filename || sizeof(filename) > sizeof(uint128_t) || used_root_entries == 128) { // Change to 128 later (need to change data type of filename maybe)
 		return -1;
@@ -270,8 +325,6 @@ int fs_create(const char *filename)
 
 int fs_delete(const char *filename)
 {
-	/* TODO: Phase 2 */
-
 	// Return -1 if no FS is mounted or filename is invalid
 	if (block_disk_count() == -1 || !filename) { // CHECK IF FILE IS CURRENTLY OPEN
 		return -1;
@@ -326,8 +379,6 @@ int fs_delete(const char *filename)
 
 int fs_ls(void)
 {
-	/* TODO: Phase 2 */
-
 	printf("FS Ls:\n");
 
 	// Return -1 if no FS is mounted
@@ -346,8 +397,6 @@ int fs_ls(void)
 
 int fs_open(const char *filename)
 {
-	/* TODO: Phase 3 */
-
 	// Return -1 if no FS is mounted
 	if (block_disk_count() == -1) {
 		return -1;
@@ -368,7 +417,7 @@ int fs_open(const char *filename)
 		if (files[i] == NULL) {
 
 			//Allocate memory
-			files[i] = (struct file_descripter*)malloc(sizeof(struct file_descripter));
+			files[i] = (struct file_descriptor*)malloc(sizeof(struct file_descriptor));
 			if (files[i] == NULL) {
 				return -1;
 			}
@@ -404,8 +453,6 @@ int fs_open(const char *filename)
 
 int fs_close(int fd)
 {
-	/* TODO: Phase 3 */
-
 	// Return -1 if no FS is mounted
 	if (block_disk_count() == -1) {
 		return -1;
@@ -421,7 +468,7 @@ int fs_close(int fd)
 		return -1;
 	}
 
-	//Remove file descripter object
+	//Remove file descriptor object
 	free(files[fd]);
 	files[fd] = NULL;
 
@@ -432,8 +479,6 @@ int fs_close(int fd)
 
 int fs_stat(int fd)
 {
-	/* TODO: Phase 3 */
-
 	// Return -1 if no FS is mounted
 	if (block_disk_count() == -1) {
 		return -1;
@@ -449,14 +494,12 @@ int fs_stat(int fd)
 		return -1;
 	}
 
-	//Find size from the file descripter which the fd knows its entry
+	//Find size from the file descriptor which the fd knows its entry
 	return rd->entries[files[fd]->root_entry].size;
 }
 
 int fs_lseek(int fd, size_t offset)
 {
-	/* TODO: Phase 3 */
-
 	// Return -1 if no FS is mounted
 	if (block_disk_count() == -1) {
 		return -1;
@@ -484,7 +527,6 @@ int fs_lseek(int fd, size_t offset)
 
 int fs_write(int fd, void *buf, size_t count)
 {
-	/* TODO: Phase 4 */
 	// Return -1 if no FS is mounted or fd is out of bounds or fd isn't open or buffer doesn't exist
 	if (block_disk_count() == -1 || fd > FS_FILE_MAX_COUNT - 1 || fd < 0 || !files[fd] || !buf) {
 		return -1;
@@ -500,10 +542,11 @@ int fs_write(int fd, void *buf, size_t count)
 	char bounce[BLOCK_SIZE * (((*file).size + count) / BLOCK_SIZE + 1)];
 	memset(bounce, '\0', sizeof(bounce));
 
+	// Holds block specific buffer
 	char block_bounce[BLOCK_SIZE];
 	int to_read = (*file).index;
 
-
+	// reading all blocks in file to bounce buffer, is this a waste reading the whole file, or should we only read what we need to modify
 	if ((*file).size) {
 		/*
 		int temp_offset = files[fd]->offset;
@@ -511,14 +554,16 @@ int fs_write(int fd, void *buf, size_t count)
 		fs_read(fd, bounce, BLOCK_SIZE * ((files[fd]->offset + count) / BLOCK_SIZE + 1));
 		fs_lseek(fd, temp_offset);
 		*/
-		while (1) {
+		int inc = 0;
+		while (++inc) {
 			//Check if blocks to read are out of bounds **Might break code**
 			if (to_read + 1 > sb->data_blk_count) {
 				break;
 			}
 
 			block_read(to_read + sb->data_blk, block_bounce);
-			strncat(bounce, block_bounce, BLOCK_SIZE);
+			//strncat(bounce, block_bounce, BLOCK_SIZE);
+			memcpy(bounce + (inc - 1) * BLOCK_SIZE, block_bounce, BLOCK_SIZE);
 			if (get_next_data_block(to_read) == FAT_EOC) {
 				break;
 			}
@@ -529,8 +574,7 @@ int fs_write(int fd, void *buf, size_t count)
 	memcpy(bounce + files[fd]->offset, buf, count);
 
 	int diff = (files[fd]->offset + count) / BLOCK_SIZE - (*file).size / BLOCK_SIZE;
-
-	
+	//printf("offset=%lu, count=%lu, size=%u, diff=%d\n", files[fd]->offset, count, (*file).size, diff);
 
 	// Runs if appending data
 	if (diff > 0 && (*file).size) {
@@ -547,6 +591,11 @@ int fs_write(int fd, void *buf, size_t count)
 
 		for (int i = 0; i < diff; i++) {
 			int free_data_block = get_free_data_block();
+
+			if (free_data_block == -1) {
+				//Fix file.size
+				break;
+			}
 			int free_fat_block = free_data_block / (BLOCK_SIZE / 2);
 			int free_fat_index = free_data_block % (BLOCK_SIZE / 2);
 
@@ -558,23 +607,35 @@ int fs_write(int fd, void *buf, size_t count)
 		}
 
 		if ((*file).size < files[fd]->offset + count) {
-			(*file).size = files[fd]->offset + count;
+			//(*file).size = files[fd]->offset + count;
 		}
 	}
 	else if (diff == 0 && (*file).size) { // Runs if appending but not enough to fill block
 		if ((*file).size < files[fd]->offset + count) {
-			(*file).size = files[fd]->offset + count;
+			//(*file).size = files[fd]->offset + count;
 		}
 	}
 	else if (!(*file).size) { // Runs if no file
-		int prev_data_block = get_free_data_block();
+		int prev_data_block = get_free_data_block(); 
+
+		if (prev_data_block == -1) {
+			return 0;
+		}
+
 		int prev_fat_block = prev_data_block / (BLOCK_SIZE / 2);
 		int prev_fat_index = prev_data_block % (BLOCK_SIZE / 2);
 		
 		fat_array[prev_fat_block].index[prev_fat_index] = FAT_EOC;
 
+		//int fail = 0;
 		for (int i = 0; i < (int)count / BLOCK_SIZE; i++) {
 			int free_data_block = get_free_data_block();
+
+			if (free_data_block == -1) {
+				//fail = 1;
+				break;
+			}
+
 			int free_fat_block = free_data_block / (BLOCK_SIZE / 2);
 			int free_fat_index = free_data_block % (BLOCK_SIZE / 2);
 
@@ -586,64 +647,92 @@ int fs_write(int fd, void *buf, size_t count)
 			prev_fat_index = free_fat_index;
 		}
 
-		(*file).size = count;
+		/*
+		if (fail) {
+			(*file).size = count - files[fd]->offset
+		}
+		else {
+			(*file).size = count;
+		}
+		*/
+		
 		(*file).index = prev_data_block;
 	}
 
+	// first data block of a file
 	int to_write = (*file).index;
 
 	int inc = 0;
 	int offset_start = files[fd]->offset;
 
-	//Used to calculate the amount of bytes that dont fill a block
+	// Used to calculate the amount of bytes that don't fill a block
 	int difference = 0;
+
+	// writes bounce buffer data to the current/new blocks
 	while ((++inc)) {
-		//Check to see if block to write is outside of file 
+
+		// Check to see if block to write is outside of max number of data blocks in file system
 		if (to_write + 1 > sb->data_blk_count) {
-			//Return bytes written
+			// bail and return curret bytes written
+			(*file).size += files[fd]->offset - offset_start;
 			return (files[fd]->offset - offset_start);
 		}
 
+		// block sized buffer
 		char block_bounce[BLOCK_SIZE];
+
+		// sets the end of buffer to null termination
 		memset(block_bounce, '\0', BLOCK_SIZE);
 
+		// copy block sized chunk from bounce to block buffer
 		memcpy(block_bounce, bounce + (inc - 1) * BLOCK_SIZE, BLOCK_SIZE);
 
+		// writes block buffer to current block
 		block_write(to_write + sb->data_blk, block_bounce);
 
-		
-
+		// checks if end of file (i.e. no more allocated data blocks) should be the same as is bounce buffer empty
 		if (get_next_data_block(to_write) == FAT_EOC) {
 			files[fd]->offset += difference;
 			break;
 		}
 
-		//Move offset to the end of the block
+		// update the offset for the first block
 		if (inc == 1) {
+			// update offset within block
 			if (count < BLOCK_SIZE - files[fd]->offset) {
+				// move the offset accordingly
 				files[fd]->offset += count;
 			}
+
+			// update offset to end of first block
 			else {
+				// TODO: do we need this
 				difference = (BLOCK_SIZE - files[fd]->offset);
+
+				// update offset to the end of the block
 				files[fd]->offset += (BLOCK_SIZE - files[fd]->offset);
 			}
 		}
+
+		// update the offset for subsequent blocks
 		else {
 			files[fd]->offset += BLOCK_SIZE;
 		}
 
+		// what's the next block to write to
 		to_write = get_next_data_block(to_write);
-
-		
 	}
 
-	files[fd]->offset += count;
-	return count; // Return number of bytes written
+	(*file).size += (files[fd]->offset - offset_start);
+
+	files[fd]->offset = offset_start + count;
+
+	return count;
 }
 
 int fs_read(int fd, void *buf, size_t count)
 {
-	/* TODO: Phase 4 */
+	//printf("fs_read: count=%lu\n", count);
 
 	// Return -1 if no FS is mounted or fd is out of bounds or fd isn't open or buffer doesn't exist
 	if (block_disk_count() == -1 || fd > FS_FILE_MAX_COUNT - 1 || fd < 0 || !files[fd] || !buf) {
@@ -666,22 +755,29 @@ int fs_read(int fd, void *buf, size_t count)
 
 	int to_read = (*file).index;
 	int inc = 0;
+	int offset_start = files[fd]->offset;
+	//printf("fs_read: offset_start=%d\n", offset_start);
 
 	// Continuously grab data until reaching FAT_EOC
 	while ((++inc)) {
+		//printf("fs_read: to_read=%d\n", to_read);
+
 		//Check if read is out of bounds
 		if (to_read + 1 > sb->data_blk_count) {
 			count = (inc-1) * BLOCK_SIZE - files[fd]->offset;
+			//printf("fs_read: read is out of bounds, count=%lu\n", count);
 			break;
 		}
 
 		block_read(to_read + sb->data_blk, block_bounce);
-		strncat(bounce, block_bounce, BLOCK_SIZE);
+		//strncat(bounce, block_bounce, BLOCK_SIZE);
+		memcpy(bounce + (inc - 1) * BLOCK_SIZE, block_bounce, BLOCK_SIZE);
 
 		//printf("%i: \n%s\n", inc, block_bounce);
 
 		// Break if reaches FAT_EOC
 		if (get_next_data_block(to_read) == FAT_EOC) {
+			//printf("fs_read: break if reaches FAT_EOC\n");
 			break;
 		}
 
@@ -689,14 +785,19 @@ int fs_read(int fd, void *buf, size_t count)
 
 		if (to_read == -1) {
 			count = inc * BLOCK_SIZE - files[fd]->offset;
+			//printf("fs_read: to_read == -1, count=%lu\n", count);
 			break;
 		}
 	}
-	
-	strncpy(buf, bounce + files[fd]->offset, count);
 
-	files[fd]->offset += count;
+	//strncpy(buf, bounce + files[fd]->offset, count);
+	memcpy(buf, bounce + offset_start, count);
 
+	//files[fd]->offset += count;
+	files[fd]->offset = offset_start + count;
+	//printf("fs_read: end_offset=%lu\n", files[fd]->offset);
+
+	//printf("fs_read: return_count=%lu\n", count);
 	return count;
 }
 
